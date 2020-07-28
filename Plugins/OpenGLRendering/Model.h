42849855b47b6465464ef5a5b659ff2896720aa0
art lora <lora.art@outlook.com>
2020-07-28 22:12:26 +0100

    some  changes

--- Plugins/OpenGLRendering/Model.h
+++ Plugins/OpenGLRendering/Model.h
@@ -215,7 +215,7 @@ namespace NCL {
 				//todo: 
 				oglMaterial = new OGLMaterial(pbrTexArry);
 				//todo:check
-				if (1) {
+				if (pbrTexArry[TextureType::METALLIC_MAP]->GetObjectID()== blackTexture->GetObjectID()) {
 					oglMaterial->roughnessValue = 0.8;
 					oglMaterial->metallicValue = 0.1;
 				}
